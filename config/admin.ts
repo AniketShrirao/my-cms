5d545c5fca3e04aef09ba152470dc59e26a8b781
AniketShrirao <ashrirao327@gmail.com>
2025-04-28 15:22:13 +0530

    refactor: modified servers config app key

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'bsXhrLhAJCRxA5cWvS6utk9IBOdW+W60sjJDSXOHcms='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'FCM19jFouKgwneVSz58Qo3hWGXRrE5dAwPZ6WiK+ho4='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'Coo/OiUIN5VwVRx8+lkg9Y4e4EHxfJLYRhIN7jyEXBA='),
     },
   },
   flags: {
--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'), // Render expects 0.0.0.0 for hosting
   port: env.int('PORT', 1337),  // Default port
+  node_version: env('NODE_VERSION', '18'), // Node version
   app: {
     keys: env.array('APP_KEYS', ['gqRWPizWrCB/sbBVlp690xl+HIPQ1UWAxrsttwjbn+I=', 'rAFHN7dFMOyCO9TbflAbMDaXYf5dqcIg1OVAXWVfy8o=']), // Replace with secure keys
   },
