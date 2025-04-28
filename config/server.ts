baea50e248694208c0754a889cc1c9deb98f7db1
AniketShrirao <ashrirao327@gmail.com>
2025-04-28 14:49:47 +0530

    refactor: modified servers config app key

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), // Render expects 0.0.0.0 for hosting
+  port: env.int('PORT', 1337),  // Default port
   app: {
-    keys: env.array('APP_KEYS', ['someRandomKeyA', 'someRandomKeyB']),
+    keys: env.array('APP_KEYS', ['gqRWPizWrCB/sbBVlp690xl+HIPQ1UWAxrsttwjbn+I=', 'rAFHN7dFMOyCO9TbflAbMDaXYf5dqcIg1OVAXWVfy8o=']), // Replace with secure keys
   },
 });
